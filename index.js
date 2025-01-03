92c6200c46f5df9fea2676858a64c8299a4feb94
Rayi <159104955+ryhazh@users.noreply.github.com>
2025-01-03 20:22:40 +0700

    update

--- index.js
+++ index.js
@@ -2,27 +2,23 @@ const express = require('express');
 const corsAnywhere = require('cors-anywhere');
 const app = express();
 
-// Create a CORS Anywhere proxy
 const proxy = corsAnywhere.createServer({
     originWhitelist: [], // Allow all origins
-
     removeHeaders: ['cookie', 'cookie2'],
     setHeaders: {
         'Access-Control-Allow-Origin': '*' // Allow all origins
     }
 });
 
-// Handle requests with a query parameter 'url'
 app.use((req, res) => {
-    // Extract and set the URL for CORS Anywhere
-    const targetUrl = req.query.url;
-    if (!targetUrl) {
-        res.status(400).send('No URL provided');
+    // Extract the target URL from the request path
+    const targetUrl = req.url.slice(1); 
+    if (!targetUrl.startsWith('http')) {
+        res.status(400).send('Invalid URL provided');
         return;
     }
 
-    // Adjust the URL for CORS Anywhere
-    req.url = targetUrl.startsWith('/') ? targetUrl : `/${targetUrl}`;
+    req.url = `/${targetUrl}`;
     proxy.emit('request', req, res);
 });
 
